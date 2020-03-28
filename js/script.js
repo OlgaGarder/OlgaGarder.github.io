28d9a0ed592ff4f06f22cc782e8999fbbe90d3e7
OlgaGarder <50359762+OlgaGarder@users.noreply.github.com>
2020-03-28 11:13:49 +0300

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$('.my_carousel_inner').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg"></button>',
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
